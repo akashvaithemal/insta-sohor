52d51b7aae355270e1638847aeecf68b4f90dcc0
akashvaithemal <sahmmed974@gmail.com>
2022-03-11 01:55:41 +0600

    love section bug and also report section extra bug solved

--- js/app.js
+++ js/app.js
@@ -146,6 +146,7 @@ const showPosts = (posts) => {
 };
 
 const displayLikedPosts = () => {
+  document.getElementById( "liked" ).innerHTML='';
     const likedPosts = getLikedPosts();
     likedPosts.forEach((post) => {
         const div = createPost(post);
@@ -154,11 +155,13 @@ const displayLikedPosts = () => {
 };
 
 const displayReportedPosts = () => {
+  document.getElementById( "reported" ).innerHTML='';
     const reportedPosts = getReportedPosts();
     reportedPosts.forEach((post) => {
         const div = createPost(post);
         document.getElementById( "reported" ).appendChild(div);
         
+         
     });
 };
 
